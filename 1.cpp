df66cd725f8ce749a9e7d9fa8b4649c03000001b
Derek <109226053+derekjtong@users.noreply.github.com>
2023-12-01 05:10:07 -0500

    Day 1 - Part 1

---
+++ 1.cpp
@@ -0,0 +1,27 @@
+#include <fstream>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ifstream inputFile;
+    inputFile.open("1.txt");
+    string line;
+    int sum = 0;
+    if (inputFile.is_open()) {
+        while (inputFile.good()) {
+            inputFile >> line;
+            int last = -1;
+            for (char c : line) {
+                if (c >= '0' && c <= '9') {
+                    if (last == -1) {
+                        sum += 10 * ((int)c - '0');
+                    }
+                    last = ((int)c - '0');
+                }
+            }
+            sum += last;
+        }
+    }
+    cout << "sum: " << sum << endl;
+}
